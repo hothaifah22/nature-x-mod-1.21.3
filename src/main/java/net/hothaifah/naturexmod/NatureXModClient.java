371004c591121b176031e526bc04826d67d33ee9
hothaifah22 <hothaifahmaen45@gmail.com>
2024-12-02 16:09:21 +0200

    Initial commit

---
+++ src/main/java/net/hothaifah/naturexmod/NatureXModClient.java
@@ -0,0 +1,10 @@
+package net.hothaifah.naturexmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NatureXModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
